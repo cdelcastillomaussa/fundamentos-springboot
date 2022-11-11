bff3cadab7a1beba6b41435c88862f7301b5fe3b
Carlos Del Castillo M <maussac13@gmail.com>
2022-11-10 23:09:38 -0500

    creacion de mis nuevas dependencias e implementar otras funcionalidades o dependencias dentro de las dependencias ya creadas

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,5 +1,7 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -10,16 +12,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
+	private MyBean myBean;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+	private MyBeanWithDependency myBeanWithDependency;
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
 		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args)  {
 		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
 	}
 }
