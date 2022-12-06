0caab77b532e2c9b8a7c99bccab5d5e509feb881
Carlos Del Castillo M <maussac13@gmail.com>
2022-12-06 17:03:39 -0500

    creacion de (servicio bajo la arquitectura REST en springboot) api rest con springboot

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/controller/UserRESTController.java
@@ -0,0 +1,29 @@
+package com.fundamentos.springboot.fundamentos.controller;
+
+import com.fundamentos.springboot.fundamentos.caseuse.GetUser;
+import com.fundamentos.springboot.fundamentos.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRESTController {
+    //create, get, delete, update
+    private GetUser getUser;
+
+    public UserRESTController(GetUser getUser) {
+        this.getUser = getUser;
+    }
+
+    @GetMapping("/")
+    List<User> get(){
+        return getUser.getAll();
+
+    }
+
+
+
+}
